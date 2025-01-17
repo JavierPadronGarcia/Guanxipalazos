c011c026e0f812b48d800a28f3f785f482f0f2f1
Javier <padrongarciajavier04@gmail.com>
2025-01-17 20:38:21 +0000

    funcionalidad básica del arma y posiciones de armas del jugador

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -1,13 +1,20 @@
-using System;
 using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
-
-    public Enum HolaEnum
+    public enum gunType
     {
-
+        alabarda,
     }
 
+    public gunType type;
 
+    public Sprite gunSprite;
+
+    public float damage, fireRate;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = gunSprite;
+    }
 }
