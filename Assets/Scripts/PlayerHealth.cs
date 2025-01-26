25d9cae9a10a9572367a00f5c82651b056f45aa1
JavierPadronGarcia <padrongarciajavier04@gmail.com>
2025-01-26 01:32:58 +0000

    Agregada funcionalidad de curas y fix de animaciones en el coin y heal

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float health = 100;
+    public float maxHealth = 100f;
+    public float health = 100f;
+
+    private void Start()
+    {
+        maxHealth = health;
+    }
 
     public void Hit(float damage)
     {
         health -= damage;
+        Debug.Log("Daño recibido: " + health);
 
         if (health <= 0)
         {
@@ -18,5 +25,32 @@ public class PlayerHealth : MonoBehaviour
     {
         Debug.Log("Jugador Muerto");
         Destroy(gameObject);
+    }
+
+    public void RestoreHealth(float heal = 0)
+    {
+        Debug.Log("Restaurando vida");
+        if (heal == 0 || (health + heal) > maxHealth)
+        {
+            health = maxHealth;
+        }
+        else
+        {
+            health += heal;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Heal"))
+        {
+            HealItemController healItemControler = collision.GetComponent<HealItemController>();
+            Debug.Log(healItemControler);
+            RestoreHealth(healItemControler.heal);
+
+            Debug.Log("Vida restaurada: " + health);
+
+            Destroy(collision.gameObject);
+        }
     }
 }
