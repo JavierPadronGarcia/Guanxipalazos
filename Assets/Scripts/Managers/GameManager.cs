c0e13b53ea8f9b2456130760dc0cbf3292565713
Javier <padrongarciajavier04@gmail.com>
2025-01-27 19:32:58 +0000

    game manager agregado

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,24 +1,25 @@
+
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static int Coins = 0;
 
+    public static bool[] player1ShowedGuns = new bool[4];
+    public static bool[] player2ShowedGuns = new bool[4];
+
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
-    }
+        player1ShowedGuns[0] = true;
+        player2ShowedGuns[0] = true;
 
-    public void CollectCoin()
-    {
-        Coins++;
-    }
-
-    public void BuyItem(int quantity)
-    {
-        if (Coins! < quantity)
+        for (int i = 1; i < 4; i++)
         {
-            Coins -= quantity;
+            player1ShowedGuns[i] = false;
+            player2ShowedGuns[i] = false;
         }
+
+        DontDestroyOnLoad(gameObject);
     }
 }
