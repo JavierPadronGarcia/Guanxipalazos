2b8535ef341f198cb8e06475f5eb8c7d742dab62
JoelMedinaFerraz <joel.merraz@gmail.com>
2025-01-30 18:43:35 +0000

    Botones Vuelta Atrás Colocados en Escenas Necesarias

--- Assets/Scripts/ButtonSwitch.cs
+++ Assets/Scripts/ButtonSwitch.cs
@@ -1,8 +1,9 @@
+using TMPro;
 using UnityEngine;
 
 public class ButtonSwitch : MonoBehaviour
 {
-    public GameObject Allies, Enemies, QR;
+    public GameObject Allies, Enemies, QR, Return;
 
     public void Switch()
     {
@@ -24,12 +25,14 @@ public class ButtonSwitch : MonoBehaviour
             Allies.SetActive(false);
             Enemies.SetActive(false);
             QR.SetActive(true);
+            Return.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "VOLVER";
         }
         else
         {
             Allies.SetActive(true);
             Enemies.SetActive(false);
             QR.SetActive(false);
+            Return.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "VER EN AR";
         }
     }
 }
