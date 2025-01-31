0d8e0adefe19f0dc62eb1f23602d3b73345ffb53
Javier <padrongarciajavier04@gmail.com>
2025-01-31 19:42:48 +0000

    Cambios en los prefabs para compras en la selección de items

--- Assets/Scripts/SelectionCardController.cs
+++ Assets/Scripts/SelectionCardController.cs
@@ -1,5 +1,7 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SelectionCardController : MonoBehaviour
 {
@@ -10,13 +12,17 @@ public class SelectionCardController : MonoBehaviour
 
     public int money;
     public TextMeshProUGUI moneyText;
+    public GameObject moneySectionGameObject;
+    public GameObject itemBoughtGameObject;
 
     public int healQuantity = 50;
 
     private EndRoundSceneCanvasController endRounCanvas;
+    private Button buttonComponent;
 
     private void Start()
     {
+        buttonComponent = GetComponent<Button>();
         endRounCanvas = GameObject.FindGameObjectWithTag("EndRoundCanvas").GetComponent<EndRoundSceneCanvasController>();
         moneyText.text = money.ToString();
     }
@@ -25,6 +31,17 @@ public class SelectionCardController : MonoBehaviour
     {
         if (GameManager.Coins - money >= 0)
         {
+            GameObject selectedObject = EventSystem.current.currentSelectedGameObject;
+            buttonComponent.interactable = false;
+            itemBoughtGameObject.SetActive(true);
+            moneySectionGameObject.SetActive(false);
+
+            if (selectedObject == gameObject)
+            {
+                EventSystem.current.SetSelectedGameObject(null);
+                SelectAnotherButton();
+            }
+
             if (isHeal)
             {
                 endRounCanvas.HealPlayer(healQuantity, money);
@@ -36,8 +53,25 @@ public class SelectionCardController : MonoBehaviour
                 endRounCanvas.BuyGun(gunName, money);
                 return;
             }
+            else
+            {
+                endRounCanvas.ImproveGun(gunAttributes, gunName, money);
+            }
+        }
+    }
 
-            endRounCanvas.ImproveGun(gunAttributes, gunName, money);
+    private void SelectAnotherButton()
+    {
+        Selectable nextSelectable = buttonComponent.FindSelectable(Vector3.left);
+
+        if (nextSelectable == null)
+        {
+            nextSelectable = buttonComponent.FindSelectable(Vector3.right);
+        }
+
+        if (nextSelectable != null)
+        {
+            EventSystem.current.SetSelectedGameObject(nextSelectable.gameObject);
         }
     }
 }
